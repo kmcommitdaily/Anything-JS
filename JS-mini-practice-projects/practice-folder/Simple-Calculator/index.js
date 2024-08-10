895e7550c0ce2b6cc24fef65255176c90fd6bd1c
kimberly galos <kmgalos0909@gmail.com>
2024-08-10 14:10:22 +0800

    simple calc: finals

--- JS-mini-practice-projects/practice-folder/Simple-Calculator/index.js
+++ JS-mini-practice-projects/practice-folder/Simple-Calculator/index.js
@@ -1,18 +1,32 @@
 const resultInput = document.querySelector('.result-input');
 const calcContainer = document.querySelector('.calc-container');
 const buttons = document.querySelectorAll('.calc-container button');
+const historyBtn = document.querySelector('.history');
+const historyContainer = document.querySelector('.history-container');
 
 const equals = document.querySelector('.equal');
 const resetBtn = document.querySelector('.reset');
-
+const operator = ['x', '+', '-', '÷'];
 let toCalculate = [];
 let currentOperation = null;
 let calculationDone = false;
 let result;
+let histories = [];
+let expression;
 resultInput.value = 0;
 
 buttons.forEach((button) => {
-  button.addEventListener('click', () => handleCalculation(button));
+  button.addEventListener('click', () => {
+    let value = button.innerHTML;
+    handleCalculation(value);
+  });
+});
+
+console.log(historyBtn.addEventListener('click', () => {}));
+
+historyBtn.addEventListener('click', () => {
+  handleHistory();
+  calculationDone = true;
 });
 
 equals.addEventListener('click', handleResult);
@@ -22,18 +36,43 @@ resetBtn.addEventListener('click', () => {
   toCalculate = [];
   currentOperation = null;
   calculationDone = false;
+  histories = [];
+  historyContainer.innerHTML = '';
+});
+
+resultInput.addEventListener('keydown', (e) => {
+  const value = e.target.value;
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    handleCalculation(value);
+    handleResult();
+    e.target.focus();
+  }
 });
 
 //functions
 
-function handleCalculation(button) {
-  const operator = ['x', '+', '-', '÷'];
-  const value = button.innerHTML;
+function handleHistory() {
+  historyContainer.innerHTML = '';
+  histories.forEach((history) => {
+    console.log(history.expression);
+    console.log(history.result);
+    const modifiedExpression = history.expression
+      .replace(/\*/g, 'x')
+      .replace(/\//g, '÷');
 
+    const historyHeader = document.createElement('h3');
+
+    historyHeader.textContent = `${modifiedExpression} = ${history.result}`;
+    historyHeader.className = 'history-area';
+
+    historyContainer.appendChild(historyHeader);
+  });
+}
+
+function handleCalculation(value) {
   if (calculationDone) {
     if (operator.includes(value)) {
-      toCalculate = [result];
-      resultInput.value = toCalculate;
       calculationDone = false;
       console.log(result);
     } else {
@@ -57,14 +96,15 @@ function handleCalculation(button) {
 
 function handleResult() {
   if (toCalculate.length > 0) {
-    let expression = toCalculate.join('');
-    console.log(expression);
+    expression = toCalculate.join('');
 
     expression = expression.replace(/x/g, '*').replace(/÷/g, '/');
 
     try {
       result = eval(expression);
       resultInput.value = Number.isInteger(result) ? result : result.toFixed(2);
+
+      console.log(histories.result);
       toCalculate = [result];
       calculationDone = true;
     } catch (error) {
@@ -73,5 +113,7 @@ function handleResult() {
       toCalculate = [];
       calculationDone = false;
     }
+
+    histories.push({ expression, result });
   }
 }
